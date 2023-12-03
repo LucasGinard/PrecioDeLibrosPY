cd85580faa40384a39376afbc69f1b31e3985bf0
LucasGinard <contactolucasginard@gmail.com>
2023-12-03 18:19:09 -0300

    add example for doc

--- backend/main.py
+++ backend/main.py
@@ -3,6 +3,10 @@ from fastapi.openapi.docs import get_swagger_ui_html, get_redoc_html
 
 from scraping import scrape_books
 
+from model.BookData import BookData
+
+from typing import List
+
 titleDoc = "PrecioDeLibrosPY API"
 urlIcon = "📚"
 
@@ -23,7 +27,7 @@ def overridden_swagger():
 def overridden_redoc():
 	return get_redoc_html(openapi_url="/openapi.json", title= titleDoc, redoc_favicon_url=urlIcon)
 
-@app.get("/search/{search_query}")
+@app.get("/search/{search_query}", response_model=List[BookData])
 async def search_books(search_query: str):
     scraped_books = scrape_books(search_query)
     return scraped_books
