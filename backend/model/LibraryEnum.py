fe6ba4dfed6931fcdd52ad2d60388441c63e4ab8
LucasGinard <contactolucasginard@gmail.com>
2024-01-13 19:10:04 -0300

    fix imports relative paths

--- backend/model/LibraryEnum.py
+++ backend/model/LibraryEnum.py
@@ -3,7 +3,7 @@ from enum import Enum
 
 from pydantic import BaseModel
 
-from ..scraping.scrapingListBooks import scrape_el_lector, scrape_lpt, scrape_mundo_libros_py
+from scraping.scrapingListBooks import scrape_el_lector, scrape_lpt, scrape_mundo_libros_py
 
 class LibraryEnum(str, Enum):
     lpt = "lpt"
